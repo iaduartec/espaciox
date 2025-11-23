faf72286b516b5102650ebaf6c3592c9174e150a
SERGIO <ia@duartec.es>
2025-11-23 19:42:33 +0100

    Update generate-page-images.mjs

--- scripts/generate-page-images.mjs
+++ scripts/generate-page-images.mjs
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { GoogleGenAI } from "@google/genai";
 import * as fs from "node:fs";
 import * as path from "node:path";
@@ -59,4 +60,3 @@ main().catch((err) => {
   console.error(err);
   process.exit(1);
 });
-
