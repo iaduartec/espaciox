16cb3ad4f4bc25b0b7a54702a12825a17713f96d
SERGIO <ia@duartec.es>
2025-11-23 14:37:23 +0100

    Add spaces.php

---
+++ backend/config/spaces.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'schedule' => [
+        'monday' => ['start' => '09:00', 'end' => '22:00'],
+        'tuesday' => ['start' => '09:00', 'end' => '22:00'],
+        'wednesday' => ['start' => '09:00', 'end' => '22:00'],
+        'thursday' => ['start' => '09:00', 'end' => '22:00'],
+        'friday' => ['start' => '09:00', 'end' => '23:00'],
+        'saturday' => ['start' => '10:00', 'end' => '23:00'],
+        'sunday' => ['start' => '10:00', 'end' => '20:00'],
+    ],
+];
