63c8b7ebacca4abe7625721013b3ed85edcc4052
SERGIO <ia@duartec.es>
2025-11-23 23:24:26 +0100

    Update RouteServiceProvider.php

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -18,6 +18,9 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
         });
     }
 
