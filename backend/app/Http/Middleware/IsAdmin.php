6d864ce18db50409d349de247a387604e6260328
SERGIO <ia@duartec.es>
2025-11-23 14:33:53 +0100

    Add IsAdmin.php

---
+++ backend/app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\\Http\\Middleware;
+
+use Closure;
+use Illuminate\\Auth\\Access\\AuthorizationException;
+use Illuminate\\Http\\Request;
+
+class IsAdmin
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $user = $request->user();
+
+        if (! $user || ! $user->is_admin) {
+            throw new AuthorizationException('Unauthorized');
+        }
+
+        return $next($request);
+    }
+}
