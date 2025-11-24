600ca2ae3360c5cb6c2112e2a091d8f057d4a462
SERGIO <ia@duartec.es>
2025-11-24 01:44:32 +0100

    Add cache.php

---
+++ backend/config/cache.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'default' => env('CACHE_STORE', env('CACHE_DRIVER', 'file')),
+
+    'stores' => [
+        'file' => [
+            'driver' => 'file',
+            'path' => storage_path('framework/cache/data'),
+        ],
+        'array' => [
+            'driver' => 'array',
+            'serialize' => false,
+        ],
+    ],
+
+    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_cache'),
+];
