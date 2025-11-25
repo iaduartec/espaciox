01394b986e8a8c15e2ccd133b086f86ae6b0c0f2
SERGIO <ia@duartec.es>
2025-11-25 23:19:52 +0100

    Add /healthz endpoint for diagnostics

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,3 +9,12 @@ Route::get('/', function () {
         'env' => config('app.env'),
     ]);
 });
+
+Route::get('/healthz', function () {
+    return response()->json([
+        'app_key_set' => (bool) config('app.key'),
+        'app_env' => config('app.env'),
+        'app_debug' => config('app.debug'),
+        'has_dotenv' => file_exists(base_path('.env')),
+    ]);
+});
