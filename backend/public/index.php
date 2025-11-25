6a3d3fb420e43d3257a296aec92e42a28c2a1150
SERGIO <ia@duartec.es>
2025-11-25 23:28:17 +0100

    Temporarily dump bootstrap exceptions

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Throwable;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -8,10 +9,17 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Request::capture()
-);
+try {
+    $response = $kernel->handle(
+        $request = Request::capture()
+    );
 
-$response->send();
+    $response->send();
 
-$kernel->terminate($request, $response);
+    $kernel->terminate($request, $response);
+} catch (Throwable $e) {
+    http_response_code(500);
+    echo "Bootstrap exception: " . $e->getMessage();
+    error_log($e);
+    throw $e;
+}
