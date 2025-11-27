58402f337ede6e6117741b19e9e36f3a324cd81b
SERGIO <ia@duartec.es>
2025-11-27 04:02:08 +0100

    Add BasicTest.php

---
+++ backend/tests/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function test_sanity(): void
+    {
+        $this->assertTrue(true);
+    }
+}
