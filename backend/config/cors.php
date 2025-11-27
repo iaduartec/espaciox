b0d0d2b95f11596efc27ac3141f17acb7d06c61c
SERGIO <ia@duartec.es>
2025-11-27 04:41:38 +0100

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,11 +1,17 @@
 <?php
 
+/* Proyecto El Santuario
+   Creado por Sergio Gómez Barrio — Duartec Instalaciones Informáticas (Burgos, España)
+*/
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'https://espaciox.vercel.app',
         'https://iaduartec.github.io',
+        'https://elsantuario.com',
+        'https://www.elsantuario.com',
         'http://localhost:8000',
         'http://localhost:8001',
         'http://127.0.0.1:8000',
