9cac87b05d6fc8da4e3039522d9f78114804961b
SERGIO <ia@duartec.es>
2025-11-27 03:30:49 +0100

    Update sanctum.php

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -3,7 +3,7 @@
 return [
     'stateful' => array_unique(array_filter(array_map('trim', explode(',', env(
         'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,127.0.0.1,localhost:8000,127.0.0.1:8000'
+        'localhost,127.0.0.1,localhost:8000,localhost:8001,127.0.0.1:8000,127.0.0.1:8001,espaciox.vercel.app'
     ))))),
     'expiration' => (int) env('SANCTUM_EXPIRATION', 120),
     'middleware' => [
