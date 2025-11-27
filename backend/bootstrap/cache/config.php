812b11bbd56dff9e34ac07232800d7c8f5139ca3
SERGIO <ia@duartec.es>
2025-11-27 01:02:30 +0100

    Add config.php

---
+++ backend/bootstrap/cache/config.php
@@ -0,0 +1,782 @@
+<?php return array (
+  'concurrency' => 
+  array (
+    'default' => 'process',
+  ),
+  'view' => 
+  array (
+    'paths' => 
+    array (
+      0 => '/home/kiri_/code/espaciox/backend/resources/views',
+    ),
+    'compiled' => false,
+  ),
+  'logging' => 
+  array (
+    'default' => 'stack',
+    'deprecations' => 
+    array (
+      'channel' => 'null',
+      'trace' => false,
+    ),
+    'channels' => 
+    array (
+      'stack' => 
+      array (
+        'driver' => 'stack',
+        'channels' => 
+        array (
+          0 => 'single',
+        ),
+        'ignore_exceptions' => false,
+      ),
+      'single' => 
+      array (
+        'driver' => 'single',
+        'path' => '/home/kiri_/code/espaciox/backend/storage/logs/laravel.log',
+        'level' => 'info',
+        'replace_placeholders' => true,
+      ),
+      'daily' => 
+      array (
+        'driver' => 'daily',
+        'path' => '/home/kiri_/code/espaciox/backend/storage/logs/laravel.log',
+        'level' => 'info',
+        'days' => 14,
+        'replace_placeholders' => true,
+      ),
+      'slack' => 
+      array (
+        'driver' => 'slack',
+        'url' => NULL,
+        'username' => 'Laravel Log',
+        'emoji' => ':boom:',
+        'level' => 'info',
+        'replace_placeholders' => true,
+      ),
+      'papertrail' => 
+      array (
+        'driver' => 'monolog',
+        'level' => 'info',
+        'handler' => 'Monolog\\Handler\\SyslogUdpHandler',
+        'handler_with' => 
+        array (
+          'host' => NULL,
+          'port' => NULL,
+          'connectionString' => 'tls://:',
+        ),
+        'processors' => 
+        array (
+          0 => 'Monolog\\Processor\\PsrLogMessageProcessor',
+        ),
+      ),
+      'stderr' => 
+      array (
+        'driver' => 'monolog',
+        'level' => 'info',
+        'handler' => 'Monolog\\Handler\\StreamHandler',
+        'formatter' => NULL,
+        'with' => 
+        array (
+          'stream' => 'php://stderr',
+        ),
+        'processors' => 
+        array (
+          0 => 'Monolog\\Processor\\PsrLogMessageProcessor',
+        ),
+      ),
+      'syslog' => 
+      array (
+        'driver' => 'syslog',
+        'level' => 'info',
+        'facility' => 8,
+        'replace_placeholders' => true,
+      ),
+      'errorlog' => 
+      array (
+        'driver' => 'errorlog',
+        'level' => 'info',
+        'replace_placeholders' => true,
+      ),
+      'null' => 
+      array (
+        'driver' => 'monolog',
+        'handler' => 'Monolog\\Handler\\NullHandler',
+      ),
+      'emergency' => 
+      array (
+        'path' => '/home/kiri_/code/espaciox/backend/storage/logs/laravel.log',
+      ),
+    ),
+  ),
+  'services' => 
+  array (
+    'postmark' => 
+    array (
+      'token' => NULL,
+    ),
+    'ses' => 
+    array (
+      'key' => NULL,
+      'secret' => NULL,
+      'region' => 'us-east-1',
+    ),
+    'resend' => 
+    array (
+      'key' => NULL,
+    ),
+    'slack' => 
+    array (
+      'notifications' => 
+      array (
+        'bot_user_oauth_token' => NULL,
+        'channel' => NULL,
+      ),
+    ),
+  ),
+  'queue' => 
+  array (
+    'default' => 'sync',
+    'connections' => 
+    array (
+      'sync' => 
+      array (
+        'driver' => 'sync',
+      ),
+      'database' => 
+      array (
+        'driver' => 'database',
+        'connection' => NULL,
+        'table' => 'jobs',
+        'queue' => 'default',
+        'retry_after' => 90,
+        'after_commit' => false,
+      ),
+      'beanstalkd' => 
+      array (
+        'driver' => 'beanstalkd',
+        'host' => 'localhost',
+        'queue' => 'default',
+        'retry_after' => 90,
+        'block_for' => 0,
+        'after_commit' => false,
+      ),
+      'sqs' => 
+      array (
+        'driver' => 'sqs',
+        'key' => NULL,
+        'secret' => NULL,
+        'prefix' => 'https://sqs.us-east-1.amazonaws.com/your-account-id',
+        'queue' => 'default',
+        'suffix' => NULL,
+        'region' => 'us-east-1',
+        'after_commit' => false,
+      ),
+      'redis' => 
+      array (
+        'driver' => 'redis',
+        'connection' => 'default',
+        'queue' => 'default',
+        'retry_after' => 90,
+        'block_for' => NULL,
+        'after_commit' => false,
+      ),
+    ),
+    'batching' => 
+    array (
+      'database' => 'pgsql',
+      'table' => 'job_batches',
+    ),
+    'failed' => 
+    array (
+      'driver' => 'database-uuids',
+      'database' => 'pgsql',
+      'table' => 'failed_jobs',
+    ),
+  ),
+  'hashing' => 
+  array (
+    'driver' => 'bcrypt',
+    'bcrypt' => 
+    array (
+      'rounds' => 12,
+      'verify' => true,
+    ),
+    'argon' => 
+    array (
+      'memory' => 65536,
+      'threads' => 1,
+      'time' => 4,
+      'verify' => true,
+    ),
+    'rehash_on_login' => true,
+  ),
+  'mail' => 
+  array (
+    'default' => 'log',
+    'mailers' => 
+    array (
+      'smtp' => 
+      array (
+        'transport' => 'smtp',
+        'scheme' => NULL,
+        'url' => NULL,
+        'host' => '127.0.0.1',
+        'port' => 2525,
+        'username' => NULL,
+        'password' => NULL,
+        'timeout' => NULL,
+        'local_domain' => 'espaciox.onrender.com',
+      ),
+      'ses' => 
+      array (
+        'transport' => 'ses',
+      ),
+      'postmark' => 
+      array (
+        'transport' => 'postmark',
+      ),
+      'resend' => 
+      array (
+        'transport' => 'resend',
+      ),
+      'sendmail' => 
+      array (
+        'transport' => 'sendmail',
+        'path' => '/usr/sbin/sendmail -bs -i',
+      ),
+      'log' => 
+      array (
+        'transport' => 'log',
+        'channel' => NULL,
+      ),
+      'array' => 
+      array (
+        'transport' => 'array',
+      ),
+      'failover' => 
+      array (
+        'transport' => 'failover',
+        'mailers' => 
+        array (
+          0 => 'smtp',
+          1 => 'log',
+        ),
+      ),
+      'roundrobin' => 
+      array (
+        'transport' => 'roundrobin',
+        'mailers' => 
+        array (
+          0 => 'ses',
+          1 => 'postmark',
+        ),
+      ),
+    ),
+    'from' => 
+    array (
+      'address' => 'hello@example.com',
+      'name' => 'Example',
+    ),
+    'markdown' => 
+    array (
+      'theme' => 'default',
+      'paths' => 
+      array (
+        0 => '/home/kiri_/code/espaciox/backend/resources/views/vendor/mail',
+      ),
+    ),
+  ),
+  'filesystems' => 
+  array (
+    'default' => 'local',
+    'disks' => 
+    array (
+      'local' => 
+      array (
+        'driver' => 'local',
+        'root' => '/home/kiri_/code/espaciox/backend/storage/app',
+        'throw' => false,
+        'report' => false,
+      ),
+      'public' => 
+      array (
+        'driver' => 'local',
+        'root' => '/home/kiri_/code/espaciox/backend/storage/app/public',
+        'url' => 'https://espaciox.onrender.com/storage',
+        'visibility' => 'public',
+        'throw' => false,
+        'report' => false,
+      ),
+      's3' => 
+      array (
+        'driver' => 's3',
+        'key' => NULL,
+        'secret' => NULL,
+        'region' => NULL,
+        'bucket' => NULL,
+        'url' => NULL,
+        'endpoint' => NULL,
+        'use_path_style_endpoint' => false,
+        'throw' => false,
+        'report' => false,
+      ),
+    ),
+    'links' => 
+    array (
+      '/home/kiri_/code/espaciox/backend/public/storage' => '/home/kiri_/code/espaciox/backend/storage/app/public',
+    ),
+  ),
+  'broadcasting' => 
+  array (
+    'default' => 'null',
+    'connections' => 
+    array (
+      'reverb' => 
+      array (
+        'driver' => 'reverb',
+        'key' => NULL,
+        'secret' => NULL,
+        'app_id' => NULL,
+        'options' => 
+        array (
+          'host' => NULL,
+          'port' => 443,
+          'scheme' => 'https',
+          'useTLS' => true,
+        ),
+        'client_options' => 
+        array (
+        ),
+      ),
+      'pusher' => 
+      array (
+        'driver' => 'pusher',
+        'key' => NULL,
+        'secret' => NULL,
+        'app_id' => NULL,
+        'options' => 
+        array (
+          'cluster' => NULL,
+          'host' => 'api-mt1.pusher.com',
+          'port' => 443,
+          'scheme' => 'https',
+          'encrypted' => true,
+          'useTLS' => true,
+        ),
+        'client_options' => 
+        array (
+        ),
+      ),
+      'ably' => 
+      array (
+        'driver' => 'ably',
+        'key' => NULL,
+      ),
+      'log' => 
+      array (
+        'driver' => 'log',
+      ),
+      'null' => 
+      array (
+        'driver' => 'null',
+      ),
+    ),
+  ),
+  'session' => 
+  array (
+    'driver' => 'file',
+    'lifetime' => 120,
+    'expire_on_close' => false,
+    'encrypt' => false,
+    'files' => '/home/kiri_/code/espaciox/backend/storage/framework/sessions',
+    'connection' => NULL,
+    'table' => 'sessions',
+    'store' => NULL,
+    'lottery' => 
+    array (
+      0 => 2,
+      1 => 100,
+    ),
+    'cookie' => 'espaciox_session',
+    'path' => '/',
+    'domain' => 'iaduartec.github.io',
+    'secure' => NULL,
+    'http_only' => true,
+    'same_site' => 'lax',
+    'partitioned' => false,
+  ),
+  'app' => 
+  array (
+    'name' => 'EspacioX',
+    'env' => 'production',
+    'debug' => false,
+    'url' => 'https://espaciox.onrender.com',
+    'frontend_url' => 'http://localhost:3000',
+    'asset_url' => NULL,
+    'timezone' => 'Europe/Madrid',
+    'locale' => 'es',
+    'fallback_locale' => 'en',
+    'faker_locale' => 'es_ES',
+    'cipher' => 'AES-256-CBC',
+    'key' => 'base64:EDp2+9vg42JjWnHCTkdvlcNUSeL3vlNEEfl3tCBOqfI=',
+    'previous_keys' => 
+    array (
+    ),
+    'maintenance' => 
+    array (
+      'driver' => 'file',
+      'store' => 'database',
+    ),
+    'providers' => 
+    array (
+      0 => 'Illuminate\\Auth\\AuthServiceProvider',
+      1 => 'Illuminate\\Broadcasting\\BroadcastServiceProvider',
+      2 => 'Illuminate\\Bus\\BusServiceProvider',
+      3 => 'Illuminate\\Cache\\CacheServiceProvider',
+      4 => 'Illuminate\\Foundation\\Providers\\ConsoleSupportServiceProvider',
+      5 => 'Illuminate\\Cookie\\CookieServiceProvider',
+      6 => 'Illuminate\\Database\\DatabaseServiceProvider',
+      7 => 'Illuminate\\Encryption\\EncryptionServiceProvider',
+      8 => 'Illuminate\\Filesystem\\FilesystemServiceProvider',
+      9 => 'Illuminate\\Foundation\\Providers\\FoundationServiceProvider',
+      10 => 'Illuminate\\Hashing\\HashServiceProvider',
+      11 => 'Illuminate\\Mail\\MailServiceProvider',
+      12 => 'Illuminate\\Pagination\\PaginationServiceProvider',
+      13 => 'Illuminate\\Pipeline\\PipelineServiceProvider',
+      14 => 'Illuminate\\Queue\\QueueServiceProvider',
+      15 => 'Illuminate\\Session\\SessionServiceProvider',
+      16 => 'Illuminate\\Translation\\TranslationServiceProvider',
+      17 => 'Illuminate\\Validation\\ValidationServiceProvider',
+      18 => 'Illuminate\\View\\ViewServiceProvider',
+      19 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+      20 => 'App\\Providers\\RouteServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'App' => 'Illuminate\\Support\\Facades\\App',
+      'Arr' => 'Illuminate\\Support\\Arr',
+      'Auth' => 'Illuminate\\Support\\Facades\\Auth',
+      'Carbon' => 'Illuminate\\Support\\Facades\\Date',
+      'Config' => 'Illuminate\\Support\\Facades\\Config',
+      'DB' => 'Illuminate\\Support\\Facades\\DB',
+      'Log' => 'Illuminate\\Support\\Facades\\Log',
+      'Route' => 'Illuminate\\Support\\Facades\\Route',
+      'Validator' => 'Illuminate\\Support\\Facades\\Validator',
+    ),
+  ),
+  'auth' => 
+  array (
+    'defaults' => 
+    array (
+      'guard' => 'web',
+      'passwords' => 'users',
+    ),
+    'guards' => 
+    array (
+      'web' => 
+      array (
+        'driver' => 'session',
+        'provider' => 'users',
+      ),
+      'api' => 
+      array (
+        'driver' => 'sanctum',
+        'provider' => 'users',
+      ),
+      'sanctum' => 
+      array (
+        'driver' => 'sanctum',
+        'provider' => NULL,
+      ),
+    ),
+    'providers' => 
+    array (
+      'users' => 
+      array (
+        'driver' => 'eloquent',
+        'model' => 'App\\Models\\User',
+      ),
+    ),
+    'passwords' => 
+    array (
+      'users' => 
+      array (
+        'provider' => 'users',
+        'table' => 'password_resets',
+        'expire' => 60,
+        'throttle' => 60,
+      ),
+    ),
+    'password_timeout' => 10800,
+  ),
+  'cache' => 
+  array (
+    'default' => 'file',
+    'stores' => 
+    array (
+      'array' => 
+      array (
+        'driver' => 'array',
+        'serialize' => false,
+      ),
+      'database' => 
+      array (
+        'driver' => 'database',
+        'connection' => NULL,
+        'table' => 'cache',
+        'lock_connection' => NULL,
+        'lock_table' => NULL,
+      ),
+      'file' => 
+      array (
+        'driver' => 'file',
+        'path' => '/home/kiri_/code/espaciox/backend/storage/framework/cache/data',
+      ),
+      'memcached' => 
+      array (
+        'driver' => 'memcached',
+        'persistent_id' => NULL,
+        'sasl' => 
+        array (
+          0 => NULL,
+          1 => NULL,
+        ),
+        'options' => 
+        array (
+        ),
+        'servers' => 
+        array (
+          0 => 
+          array (
+            'host' => '127.0.0.1',
+            'port' => 11211,
+            'weight' => 100,
+          ),
+        ),
+      ),
+      'redis' => 
+      array (
+        'driver' => 'redis',
+        'connection' => 'cache',
+        'lock_connection' => 'default',
+      ),
+      'dynamodb' => 
+      array (
+        'driver' => 'dynamodb',
+        'key' => NULL,
+        'secret' => NULL,
+        'region' => 'us-east-1',
+        'table' => 'cache',
+        'endpoint' => NULL,
+      ),
+      'octane' => 
+      array (
+        'driver' => 'octane',
+      ),
+    ),
+    'prefix' => 'espaciox_cache',
+  ),
+  'cors' => 
+  array (
+    'paths' => 
+    array (
+      0 => 'api/*',
+      1 => 'sanctum/csrf-cookie',
+    ),
+    'allowed_methods' => 
+    array (
+      0 => '*',
+    ),
+    'allowed_origins' => 
+    array (
+      0 => 'https://elsantuario.com',
+      1 => 'https://www.elsantuario.com',
+      2 => 'https://espaciox.infinityfreeapp.com',
+      3 => 'https://iaduartec.github.io/espaciox/reservas.html',
+      4 => 'https://iaduartec.github.io',
+      5 => 'http://localhost:8000',
+      6 => 'http://localhost:3000',
+    ),
+    'allowed_origins_patterns' => 
+    array (
+    ),
+    'allowed_headers' => 
+    array (
+      0 => '*',
+    ),
+    'exposed_headers' => 
+    array (
+    ),
+    'max_age' => 0,
+    'supports_credentials' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'pgsql',
+    'connections' => 
+    array (
+      'sqlite' => 
+      array (
+        'driver' => 'sqlite',
+        'url' => NULL,
+        'database' => 'bbdd_espaciox',
+        'prefix' => '',
+        'foreign_key_constraints' => true,
+      ),
+      'mysql' => 
+      array (
+        'driver' => 'mysql',
+        'url' => NULL,
+        'host' => 'dpg-d4j4it95pdvs739681ng-a',
+        'port' => '5432',
+        'database' => 'bbdd_espaciox',
+        'username' => 'bbdd_espaciox_user',
+        'password' => 'LEY252btPS0kSTqVSJ39Hv6yYNKOBwqV',
+        'unix_socket' => '',
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'strict' => true,
+        'engine' => NULL,
+      ),
+      'mariadb' => 
+      array (
+        'driver' => 'mariadb',
+        'url' => NULL,
+        'host' => 'dpg-d4j4it95pdvs739681ng-a',
+        'port' => '5432',
+        'database' => 'bbdd_espaciox',
+        'username' => 'bbdd_espaciox_user',
+        'password' => 'LEY252btPS0kSTqVSJ39Hv6yYNKOBwqV',
+        'unix_socket' => '',
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'strict' => true,
+        'engine' => NULL,
+        'options' => 
+        array (
+        ),
+      ),
+      'pgsql' => 
+      array (
+        'driver' => 'pgsql',
+        'url' => NULL,
+        'host' => 'dpg-d4j4it95pdvs739681ng-a',
+        'port' => '5432',
+        'database' => 'bbdd_espaciox',
+        'username' => 'bbdd_espaciox_user',
+        'password' => 'LEY252btPS0kSTqVSJ39Hv6yYNKOBwqV',
+        'charset' => 'utf8',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'search_path' => 'public',
+        'sslmode' => 'prefer',
+      ),
+      'sqlsrv' => 
+      array (
+        'driver' => 'sqlsrv',
+        'url' => NULL,
+        'host' => 'dpg-d4j4it95pdvs739681ng-a',
+        'port' => '5432',
+        'database' => 'bbdd_espaciox',
+        'username' => 'bbdd_espaciox_user',
+        'password' => 'LEY252btPS0kSTqVSJ39Hv6yYNKOBwqV',
+        'charset' => 'utf8',
+        'prefix' => '',
+        'prefix_indexes' => true,
+      ),
+    ),
+    'migrations' => 'migrations',
+    'redis' => 
+    array (
+      'client' => 'phpredis',
+      'options' => 
+      array (
+        'cluster' => 'redis',
+        'prefix' => 'espaciox_database_',
+        'persistent' => false,
+      ),
+      'default' => 
+      array (
+        'url' => NULL,
+        'host' => '127.0.0.1',
+        'username' => NULL,
+        'password' => NULL,
+        'port' => '6379',
+        'database' => '0',
+      ),
+      'cache' => 
+      array (
+        'url' => NULL,
+        'host' => '127.0.0.1',
+        'username' => NULL,
+        'password' => NULL,
+        'port' => '6379',
+        'database' => '1',
+      ),
+    ),
+  ),
+  'sanctum' => 
+  array (
+    'stateful' => 
+    array (
+      0 => 'iaduartec.github.io',
+    ),
+    'guard' => 
+    array (
+      0 => 'web',
+    ),
+    'expiration' => 120,
+    'token_prefix' => '',
+    'middleware' => 
+    array (
+      'verify_csrf_token' => 'App\\Http\\Middleware\\VerifyCsrfToken',
+      'encrypt_cookies' => 'Illuminate\\Cookie\\Middleware\\EncryptCookies',
+    ),
+  ),
+  'spaces' => 
+  array (
+    'schedule' => 
+    array (
+      'monday' => 
+      array (
+        'start' => '09:00',
+        'end' => '22:00',
+      ),
+      'tuesday' => 
+      array (
+        'start' => '09:00',
+        'end' => '22:00',
+      ),
+      'wednesday' => 
+      array (
+        'start' => '09:00',
+        'end' => '22:00',
+      ),
+      'thursday' => 
+      array (
+        'start' => '09:00',
+        'end' => '22:00',
+      ),
+      'friday' => 
+      array (
+        'start' => '09:00',
+        'end' => '23:00',
+      ),
+      'saturday' => 
+      array (
+        'start' => '10:00',
+        'end' => '23:00',
+      ),
+      'sunday' => 
+      array (
+        'start' => '10:00',
+        'end' => '20:00',
+      ),
+    ),
+  ),
+);
